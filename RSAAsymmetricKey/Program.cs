52958989295088894bd3f6f1253318c97f9c6d1f
ferddev21 <ferdian.0571@students.amikom.ac.id>
2021-11-09 10:32:10 +0700

    first commit

---
+++ RSAAsymmetricKey/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RSAAsymmetricKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //RSA Encryption with Twileloop.ExpressSecurity
+
+            //1. Generate a key pair !create folder Demo
+            ExpressEncription.RSAEncription.MakeKey(@"C:\Demo\public.key", @"C:\Demo\private.key");
+
+            //2. Encrypt
+            var plainText = "hello world!!";
+            var EncryptText = ExpressEncription.RSAEncription.EncryptString(plainText, @"C:\Demo\public.key");
+            Console.WriteLine(EncryptText);
+
+            //3.Decrypt
+            var DecryptText = ExpressEncription.RSAEncription.DecryptString(EncryptText, @"C:\Demo\private.key");
+            Console.WriteLine(DecryptText);
+
+
+        }
+    }
+}
